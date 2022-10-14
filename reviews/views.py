2b3b4b1005687aef0781671b844b000a6fb01e45
MoonSanghee <psycho13th@gmail.com>
2022-10-14 17:09:11 +0900

    in reviews add update, delete

--- reviews/views.py
+++ reviews/views.py
@@ -30,3 +30,23 @@ def detail(request, pk):
     review = Review.objects.get(pk=pk)
     context = {"review": review}
     return render(request, "reviews/detail.html", context)
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    if request.method == "POST":
+        update_form = ReviewForm(request.POST, instance=review)
+        if update_form.is_valid():
+            update_form.save()
+            return redirect("reviews:index")
+    else:
+        update_form = ReviewForm(instance=review)
+
+    context = {
+        "update_form": update_form,
+    }
+    return render(request, "reviews/update.html", context)
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    review.delete()
+    return redirect('reviews:index')
