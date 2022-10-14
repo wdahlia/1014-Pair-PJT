d81de87213be96267d3b8469b030c5229d9b14bd
MoonSanghee <psycho13th@gmail.com>
2022-10-14 15:06:52 +0900

    template/navbar add

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("logout/", views.logout, name="logout"),
     path("<int:pk>/detail/", views.detail, name="detail"),
     path("<int:pk>/update/", views.update, name="update"),
+    path('diff/', views.diff, name='diff'),
 ]
