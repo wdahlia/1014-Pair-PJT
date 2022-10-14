a7b44dcc0fd12387e8bf2643c0ce40be7db32661
leesc8408 <leesc8408@gmail.com>
2022-10-14 16:45:19 +0900

    create_detail

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'reviews'
+app_name = "reviews"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('create/', views.create, name="create"),
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/detail/", views.detail, name="detail"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -4,23 +4,29 @@ from .forms import ReviewForm
 
 # Create your views here.
 
+
 def index(request):
-    review_list = Review.objects.order_by('-id')
-    context = {
-        'review_list' : review_list
-    }
-    return render(request, 'reviews/index.html', context)
+    review_list = Review.objects.order_by("-id")
+    context = {"review_list": review_list}
+    return render(request, "reviews/index.html", context)
+
 
 def create(request):
     if request.method == "POST":
         create_review = ReviewForm(request.POST)
         if create_review.is_valid():
             create_review.save()
-            return redirect('reviews:index')
+            return redirect("reviews:index")
     else:
         create_review = ReviewForm()
-    
+
     context = {
-        'create_review' : create_review,
+        "create_review": create_review,
     }
-    return render(request, 'reviews/create.html', context)
+    return render(request, "reviews/create.html", context)
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {"review": review}
+    return render(request, "reviews/detail.html", context)
