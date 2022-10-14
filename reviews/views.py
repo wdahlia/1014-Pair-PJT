ff9be90e95a04c3e64ea2d750b8ac743512cf3ef
dahlia <wlstnrdahlia@gmail.com>
2022-10-14 16:26:02 +0900

    create review app, create, index

---
+++ reviews/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
+
+# Create your views here.
+
+def index(request):
+    review_list = Review.objects.order_by('-id')
+    context = {
+        'review_list' : review_list
+    }
+    return render(request, 'reviews/index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        create_review = ReviewForm(request.POST)
+        if create_review.is_valid():
+            create_review.save()
+            return redirect('reviews:index')
+    else:
+        create_review = ReviewForm()
+    
+    context = {
+        'create_review' : create_review,
+    }
+    return render(request, 'reviews/create.html', context)
