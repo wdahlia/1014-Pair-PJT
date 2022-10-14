87f3419604b469dc247060bec2f34819dcd3bd15
leesc8408 <leesc8408@gmail.com>
2022-10-14 14:45:20 +0900

    create_update

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,11 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('', views.main, name="main"),
+    path("", views.main, name="main"),
     path("index/", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('<int:pk>/detail/', views.detail, name="detail"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("<int:pk>/detail/", views.detail, name="detail"),
+    path("<int:pk>/update/", views.update, name="update"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
@@ -8,14 +8,13 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def main(request):
-    return render(request, 'accounts/main.html')
+    return render(request, "accounts/main.html")
+
 
 def index(request):
-    account_list = get_user_model().objects.order_by('-id')
+    account_list = get_user_model().objects.order_by("-id")
 
-    context = {
-        'account_list' : account_list
-    }
+    context = {"account_list": account_list}
     return render(request, "accounts/index.html", context)
 
 
@@ -30,29 +29,42 @@ def signup(request):
     context = {"account_form": account_form}
     return render(request, "accounts/signup.html", context)
 
+
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
             return redirect("accounts:index")
     else:
         form = AuthenticationForm()
-    context = {
-        'form' : form
-    }
-    return render(request, 'accounts/login.html', context)
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
 
 def logout(request):
     auth_logout(request)
-    return redirect('accounts:main')
+    return redirect("accounts:main")
+
 
 @login_required
 def detail(request, pk):
     account_detail = get_user_model().objects.get(pk=pk)
 
-    context = {
-        'account_detail' : account_detail
-    }
+    context = {"account_detail": account_detail}
 
-    return render(request, 'accounts/detail.html', context)
+    return render(request, "accounts/detail.html", context)
+
+
+@login_required
+def update(request, pk):
+    update_user = get_user_model().objects.get(pk=pk)
+    if request.method == "POST":
+        update_form = CustomUserChangeForm(request.POST, instance=update_user)
+        if update_form.is_valid():
+            update_form.save()
+            return redirect("accounts:detail", update_user.pk)
+    else:
+        update_form = CustomUserChangeForm(instance=update_user)
+    context = {"update_form": update_form}
+    return render(request, "accounts/update.html", context)
